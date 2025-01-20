0e0268814b492aaded23b1ce2045c44f8bd1a867
Jung <y.jung@zenrin-ms.co.jp>
2025-01-20 17:31:38 +0900

    feat: layout setting

--- django_crafting/settings.py
+++ django_crafting/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_crafting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'main/templates/main')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# In local server,
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -8,14 +8,14 @@ def index(request):
 def board(request):
     postlist = Post.objects.all()
     
-    return render(request, 'main/board.html', {'postlist': postlist})
+    return render(request, 'main/board/board.html', {'postlist': postlist})
 
 def posting(request, pk):
     # pk를 통하여 게시글을 탐색
     post = Post.objects.get(pk=pk)
     
     # 템플릿에 게시글 객체를 post라는 이름으로 전달하여 렌더링
-    return render(request, 'main/posting.html', {'post': post})
+    return render(request, 'main/board/posting.html', {'post': post})
 
 def post(request):
     if request.method == 'POST':
@@ -32,11 +32,11 @@ def post(request):
                 mainphoto=request.POST['mainphoto'],
             )
         return redirect('/board/')
-    return render(request, 'main/post.html')
+    return render(request, 'main/board/post.html')
 
 def remove(request, pk):
     post = Post.objects.get(pk=pk)
     if request.method == 'POST':
         post.delete()
         return redirect('/board/')
-    return render(request, 'main/remove.html', {'post': post})
+    return render(request, 'main/board/remove.html', {'post': post})
